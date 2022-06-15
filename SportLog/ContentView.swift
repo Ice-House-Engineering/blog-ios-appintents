e9334efa6f4563fcce34d2e65034f03943c0e6c6
Rahman Habibi <rahman.habibi@icehousecorp.com>
2022-06-15 12:05:56 +0700

    Initial Commit

---
+++ SportLog/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SportLog
+//
+//  Created by Habibi on 15/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SportLog/SportLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportLogApp.swift
+//  SportLog
+//
+//  Created by Habibi on 15/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SportLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
